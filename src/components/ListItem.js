74a26684eaf40f6a85ece29f46aaaaa55ff83286
matias <matiaskochman@gmail.com>
2018-02-06 10:04:22 +0100

    getting info from api

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,48 +1,51 @@
 import React, { Component } from 'react';
-//import { Actions } from 'react-native-router-flux';
 import { Text, TouchableWithoutFeedback, View } from 'react-native';
-//import { CardSection } from './common';
-
+import moment from 'moment'
 const styles = { titleStyle: { fontSize: 16, paddingLeft: 15 } };
 
 class ListItem extends Component {
   onRowPress() {
-    //Actions.employeeEdit({ employee: this.props.employee });
+    
   }
 
   render() {
-    //const { name } = this.props.employee;
-    const { name } = this.props.post.item;
+    const { thumbnail, author, created_utc, title, score, num_comments } = this.props.post.item.data;
+
+    let date = moment.utc(created_utc).format('YYYY-MM-DD hh:mm:ss');
 
     return (
+      <View>
+        <View style={{flexDirection:'row',flex:1,backgroundColor:'#cecece',minHeight:80}}>
+          <View style={{backgroundColor:'#bababa',flex:1}}>
+          </View>
+          <View style={{flex:3,backgroundColor:'green'}}>
+            <View style={{flex:1,backgroundColor:'red',flexDirection:'row'}}>
+              <View style={{flex:1}}>
+              </View>
+              <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>date</Text>
 
-      <View style={{flexDirection:'row',flex:1,backgroundColor:'#cecece',minHeight:80}}>
-        <View style={{backgroundColor:'#bababa',flex:1}}>
+              </View>
+            </View>
+            <View style={{flex:1.3,backgroundColor:'grey',justifyContent:'center',alignItems:'center'}}>
+              <Text>Title</Text>
+            </View>
+            <View style={{flex:1,backgroundColor:'blue',flexDirection:'row'}}>
+              <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                <Text>author</Text>
+              </View>
+              <View style={{flex:1,flexDirection:'row'}}>
+                <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                  <Text>score</Text>
+                </View>
+                <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                  <Text>comments</Text>
+                </View>
+              </View>
+            </View>
+          </View>
         </View>
-        <View style={{flex:3,backgroundColor:'green'}}>
-          <View style={{flex:1,backgroundColor:'red',flexDirection:'row'}}>
-            <View style={{flex:1}}>
-            </View>
-            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-              <Text>date</Text>
-            </View>
-          </View>
-          <View style={{flex:1.3,backgroundColor:'grey',justifyContent:'center',alignItems:'center'}}>
-            <Text>Title</Text>
-          </View>
-          <View style={{flex:1,backgroundColor:'blue',flexDirection:'row'}}>
-            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-              <Text>author</Text>
-            </View>
-            <View style={{flex:1,flexDirection:'row'}}>
-              <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                <Text>score</Text>
-              </View>
-              <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                <Text>comments</Text>
-              </View>
-            </View>
-          </View>
+        <View style={{height:5,backgroundColor:'white'}}>
         </View>
       </View>
     );
--- src/containers/redditPostsContainer/sagas.js
+++ src/containers/redditPostsContainer/sagas.js
@@ -2,34 +2,27 @@ import { delay } from 'redux-saga';
 import { put, takeEvery, all, call,takeLatest  } from 'redux-saga/effects';
 import axios from 'axios';
 
-
 function* fetchPostsAsync(action) {
 
   const reddit_api = 'https://api.reddit.com/r/pics/new.json';
   let reddit_posts = yield call(axios.get,reddit_api);
   try{
-
     yield put(
         {
             type:'FETCH_POSTS_LIST',
             payload:reddit_posts.data.data.children
         }
     )
-
   } catch (e){
-
     console.error(e);
   }
 }
 
 
-// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
 function* watchFetchFechasListAsync() {
   yield takeLatest('POSTS_FETCH_SAGA', fetchPostsAsync)
 }
 
-// notice how we now only export the rootSaga
-// single entry point to start all Sagas at once
 export default function* postsSaga() {
   yield all([
     watchFetchFechasListAsync()
